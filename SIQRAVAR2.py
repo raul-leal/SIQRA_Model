91b747b3b0d4bcd80e2849a70ee840e17aacc97d
raul-leal <108129342+raul-leal@users.noreply.github.com>
2024-10-10 10:36:42 -0300

    The single parameter variation models
    Will ask for an input with the name of the parameter to vary, with the range of variation being hardcoded.

---
+++ SIQRAVAR2.py
@@ -0,0 +1,82 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+N = 100
+S0 = 99
+I0 = 1
+Q0 = 0
+R0 = 0
+A0 = 0
+
+# Select what parameter to study
+parameter = str(input('Escolha qual parâmetro variar (escreva igual está no código): '))
+
+fig = plt.figure(facecolor="w")
+ax = fig.add_subplot(111, facecolor = 'w', axisbelow = True)
+ax.set_title(f'Parâmetro variado = {parameter}')
+ax.set_xlabel('Time')
+ax.set_xlim(0, 10)
+ax.set_ylabel('Population')
+ax.set_ylim(0, 100)
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+
+for i in np.arange(0.1, 1.1, 0.1):
+    if parameter == 'beta':
+        beta = round(i,2)
+    else: beta = 0.1
+    if parameter == 'delta':
+        delta = round(i,2)
+    else: delta = 9
+    if parameter == 'alfa':
+        alfa = round(i,2)
+    else: alfa = 0.5
+    if parameter == 'sigma':
+        sigma = round(i, 2)
+    else: sigma = 0.8
+    if parameter == 'omega':
+        omega = round(i,2)
+    else: omega = 2
+    if parameter == 'alfaSA':
+        alfaSA = round(i,2)
+    else: alfaSA = 0.025
+    if parameter == 'alfaIA':
+        alfaIA = round(i,2)
+    else: alfaIA = 0.25
+    if parameter == 'alfaQA':
+        alfaQA = round(i,2)
+    else: alfaQA = 0.5
+    
+            
+
+    # A grid of time points
+    t = np.linspace(0, 10)
+
+    # The SIQRA model differential equations
+    def SIQRA(y, t, N, beta, delta, alfa, sigma, omega, 
+            alfaSA, alfaIA, alfaQA):
+        S, I, Q, R, A = y
+        dSdt = -alfaSA * S * A - beta * S * I + sigma * R + omega * Q
+        dIdt = beta * S * I - alfaIA * A * I - delta * Q * I
+        dQdt = delta * Q * I - omega * Q - alfaQA * Q - alfa * Q
+        dRdt = alfa * Q - sigma * R
+        dAdt = alfaSA * S * A + alfaIA * A * I + alfaQA * Q
+        return dSdt, dIdt, dQdt, dRdt, dAdt
+
+    # Initial conditions vector
+    y0 = S0, I0, Q0, R0, A0
+    # Integrate the SIQRA equations over the time grid, t
+    ret = odeint(SIQRA, y0, t, args=(N, beta,  delta, alfa,
+                                    sigma, omega, alfaSA, alfaIA, alfaQA))
+    S, I, Q, R, A = ret.T
+
+    
+    # Plot the data on five different curves for S(t), I(t), Q(t), R(t), A(t)
+    ax.plot(t, S, label='Susceptible')
+    ax.plot(t, I, '--', label='Infected')
+    ax.plot(t, Q, '.-', label='Quarantine')
+    ax.plot(t, R, '*', label='Removed')
+    ax.plot(t, A, 'o', label='Antidotal')
+
+plt.show()
